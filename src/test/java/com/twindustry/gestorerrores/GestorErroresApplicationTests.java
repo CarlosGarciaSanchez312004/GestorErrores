860e2ef838904ccc1babbea26e0f8ea43a4b0291
CarlosGarciaSanchez312004 <carlitogarciasanchez312004@gmail.com>
2026-05-13 14:54:32 +0200

    creacion del proyecto

---
+++ src/test/java/com/twindustry/gestorerrores/GestorErroresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twindustry.gestorerrores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorErroresApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
