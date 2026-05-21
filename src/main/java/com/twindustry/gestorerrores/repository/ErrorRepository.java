06bd82459f6e7b5199b02a1ed78dc48227f212a2
CarlosGarciaSanchez312004 <carlitogarciasanchez312004@gmail.com>
2026-05-21 11:16:01 +0200

    arreglar import y finalizar aplicacion hasta repositorios

--- src/main/java/com/twindustry/gestorerrores/repository/ErrorRepository.java
+++ src/main/java/com/twindustry/gestorerrores/repository/ErrorRepository.java
@@ -2,6 +2,6 @@ package com.twindustry.gestorerrores.repository;
 import com.twindustry.gestorerrores.specification.ErrorSpecification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
+import com.twindustry.gestorerrores.model.Error;
 public interface ErrorRepository extends JpaRepository<Error, Long> , JpaSpecificationExecutor<ErrorSpecification>{
 }
