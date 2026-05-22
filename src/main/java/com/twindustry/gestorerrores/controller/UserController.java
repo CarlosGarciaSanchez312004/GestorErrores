6ee58c82fa939166f89312c09bb1550c73b1fc93
CarlosGarciaSanchez312004 <carlitogarciasanchez312004@gmail.com>
2026-05-22 15:13:07 +0200

    User Controller

--- src/main/java/com/twindustry/gestorerrores/controller/UserController.java
+++ src/main/java/com/twindustry/gestorerrores/controller/UserController.java
@@ -1,4 +1,49 @@
 package com.twindustry.gestorerrores.controller;
 
+import com.twindustry.gestorerrores.model.User;
+import com.twindustry.gestorerrores.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
 public class UserController {
+    private final UserService userService;
+
+    @PostMapping("/create")
+    public User createUser(@RequestBody User user){
+    return userService.createUser(
+            user.getNombre(),
+            user.getApellido(),
+            user.getEmail(),
+            user.getPicture(),
+            user.getPassword(),
+            user.getRole()
+    );
+    }
+
+    @PutMapping("/{id}/enable")
+    public User enableUser(@PathVariable Long id) {
+        return userService.enableUser(id);
+
+    }
+    @PutMapping("/{id}/disable")
+    public User disable (@PathVariable Long id) {
+        return userService.disableUser(id);
+    }
+    @GetMapping("/findAll")
+    public List<User> findAllUser(){
+        return userService.findAll();
+    }
+    @GetMapping("/find/{id}")
+    public User getUser(@PathVariable Long id){
+        return userService.findById(id);
+    }
+    @GetMapping("/search")
+    public List<User> getUserByName(@RequestParam String nombre){
+        return userService.findByName(nombre);
+    }
 }
